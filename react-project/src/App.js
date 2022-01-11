fb3da2ed7a1542f91b4f4666a49e207487e79a6c
Mahnoor Khan <mahnoorza001@gmail.com>
2022-01-10 18:35:03 -0600

    first React Project

---
+++ react-project/src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './App.css';
+import NavBar from './components/navbar';
+import Counters from './components/counters'
+
+class App extends Component {
+  state={
+    counters:[{id : 1 , value: 1},
+       {id : 2 , value: 4},
+       {id : 3 , value: 3},
+       {id : 4 , value: 2}]
+}
+constructor(){
+  super()
+  // console.log('App-Constructed')
+}
+handleIncrement = counter =>{
+    const counters=[...this.state.counters];
+    const index=counters.indexOf(counter)
+    counters[index]={...counter};
+    counters[index].value++;
+    this.setState({counters});
+}
+handleDecrement = counter =>{
+  const counters=[...this.state.counters];
+  const index=counters.indexOf(counter)
+  counters[index]={...counter};
+  counters[index].value--;
+  this.setState({counters});
+}
+handleReset=()=>{
+    const counters = this.state.counters.map( c => {
+        c.value= 0
+        return c;
+   });
+   this.setState({counters})
+}
+handleDelete=(counterId)=>{
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({counters})
+}
+componentDidMount(){
+  // console.log('App-Mounted')
+}
+  render(){
+    // console.log('App-Rendered')
+  return (
+    <React.Fragment>
+  <NavBar totalCounters={this.state.counters.filter(c => c.value > 0 ).length}/>
+    <main className='container'>
+      <Counters counters={this.state.counters} onReset={this.handleReset} onIncrement={this.handleIncrement} onDelete={this.handleDelete} onDecrement={this.handleDecrement}/>
+    </main>
+    </React.Fragment>
+    );}
+}
+
+export default App;
